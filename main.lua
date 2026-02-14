6fe992111f7a3d5b79ad00874e893dadd0ed3763
codalinerev <davali2004@yahoo.com>
2026-02-14 08:56:56 +0100

    add GUI

--- main.lua
+++ main.lua
@@ -1,5 +1,25 @@
 love.graphics.setDefaultFilter("nearest")
 love.graphics.setBackgroundColor(0.4, 0.1, 0.2)
+
+txt = {}
+txt.message = "press space to start"
+txt.x = 500
+txt.y = 200
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
 function love.draw()
     love.graphics.print("HELLO!  je commence", 100, 150, 0, 2, 2)
+    love.graphics.rectangle("line", txt.x - 20, txt.y - 20, 200, 100)
+    love.graphics.print(txt.message, txt.x, txt.y, 0, 1, 1)
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        txt.message = "space pressed"
+    end
 end
