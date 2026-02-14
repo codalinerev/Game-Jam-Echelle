03c5279b3016fbe149c91ae8ca2e24cfcdf2b522
codalinerev <davali2004@yahoo.com>
2026-02-14 08:39:42 +0100

    first commit

---
+++ main.lua
@@ -0,0 +1,5 @@
+love.graphics.setDefaultFilter("nearest")
+love.graphics.setBackgroundColor(0.4, 0.1, 0.2)
+function love.draw()
+    love.graphics.print("HELLO!  je commence", 100, 150, 0, 2, 2)
+end
